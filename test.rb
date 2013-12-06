f3d5ab1a68662436756ffff00d8ddad09768edca
Ryunosuke SATO <tricknotes.rs@gmail.com>
2013-12-07 03:42:20 +0900

    Update config for TrifleJS

--- test.rb
+++ test.rb
@@ -2,14 +2,17 @@ require 'bundler/setup'
 
 Bundler.require
 
+Capybara.register_driver :trifleJS do |app|
+  Capybara::Poltergeist::Driver.new(app, path: 'TrifleJS.exe', phantomjs_options: ['--emulate:IE8'])
+end
+
 ##
 #  Override constants for trifleJS
-Capybara::Poltergeist::Client::PHANTOMJS_NAME = 'TrifleJS.exe'
-Capybara::Poltergeist::Client::PHANTOMJS_VERSION = ['~ 1.7.0']
+Capybara::Poltergeist::Client::PHANTOMJS_VERSION = ['>= 1.0.0']
 
 Capybara.app_host = 'http://www.google.com'
 
-session = Capybara::Session.new(:poltergeist)
+session = Capybara::Session.new(:trifleJS)
 
 session.visit '/'
 
