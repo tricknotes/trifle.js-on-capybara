75ace66d0d516c81645a4f0b4eeefcd6f12c5332
Ryunosuke SATO <tricknotes.rs@gmail.com>
2013-12-07 03:21:08 +0900

    Add configuration for trifleJS

--- test.rb
+++ test.rb
@@ -2,6 +2,11 @@ require 'bundler/setup'
 
 Bundler.require
 
+##
+#  Override constants for trifleJS
+Capybara::Poltergeist::Client::PHANTOMJS_NAME = 'TrifleJS.exe'
+Capybara::Poltergeist::Client::PHANTOMJS_VERSION = ['~ 1.7.0']
+
 Capybara.app_host = 'http://www.google.com'
 
 session = Capybara::Session.new(:poltergeist)
