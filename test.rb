25408f09d328f9d6bfb817040f285318a42e61dd
Ryunosuke SATO <tricknotes.rs@gmail.com>
2013-12-07 03:44:17 +0900

    `:path` option doesn't work??

--- test.rb
+++ test.rb
@@ -3,11 +3,12 @@ require 'bundler/setup'
 Bundler.require
 
 Capybara.register_driver :trifleJS do |app|
-  Capybara::Poltergeist::Driver.new(app, path: 'TrifleJS.exe', phantomjs_options: ['--emulate:IE8'])
+  Capybara::Poltergeist::Driver.new(app, phantomjs_options: ['--emulate:IE8'])
 end
 
 ##
 #  Override constants for trifleJS
+Capybara::Poltergeist::Client::PHANTOMJS_NAME = 'TrifleJS.exe'
 Capybara::Poltergeist::Client::PHANTOMJS_VERSION = ['>= 1.0.0']
 
 Capybara.app_host = 'http://www.google.com'
