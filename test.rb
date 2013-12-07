afa09b0c7837a8fdc1a6bf09063ce38d36c8e585
Ryunosuke SATO <tricknotes.rs@gmail.com>
2013-12-07 17:04:26 +0900

    Fit path to phantomjs_options to remove dirty hack

--- test.rb
+++ test.rb
@@ -2,20 +2,9 @@ require 'bundler/setup'
 
 Bundler.require
 
-##
-#  :see_no_evil:
-def Cliver::detect!(*args)
-  File.join(__dir__, args[0])
-end
-
 Capybara.register_driver :trifleJS do |app|
-  Capybara::Poltergeist::Driver.new(app, phantomjs_options: ['--emulate:IE8'])
+  Capybara::Poltergeist::Driver.new(app, phantomjs: 'TrifleJS.exe', phantomjs_options: ['--emulate:IE8'])
 end
-
-##
-#  Override constants for trifleJS
-Capybara::Poltergeist::Client::PHANTOMJS_NAME = 'TrifleJS.exe'
-Capybara::Poltergeist::Client::PHANTOMJS_VERSION = ['>= 1.0.0']
 
 Capybara.app_host = 'http://www.google.com'
 
